0871218a0d22f6b75e06a18c7fcc4c66dcdf985d
karam mustafa <karam2mustafa@gmail.com>
2020-10-02 23:04:51 +0300

    add dynamic function load all route with urls from credential.json file

--- src/Routes/ApisGeneratorRoutes.php
+++ src/Routes/ApisGeneratorRoutes.php
@@ -2,10 +2,20 @@
 
 namespace KMLaravel\ApiGenerator\Routes;
 
+use Illuminate\Support\Facades\Route;
+use KMLaravel\ApiGenerator\Facade\KMFileHelper;
+
 class ApisGeneratorRoutes
 {
     public static function getRoutes()
     {
-        return require "Routes.php";
+       return Route::group([] , function () {
+           $extraRoutes = KMFileHelper::getCredentialJsonFileAsJson();
+           foreach ($extraRoutes as $api) {
+               foreach ($api as $value) {
+                   Route::apiResource($value->url, $value->route);
+               }
+           }
+       });
     }
 }
