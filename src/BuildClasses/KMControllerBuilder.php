7b99e1cff634b8ad2dfebafa3f0319cf7ea314e7
karam mustafa <karam2mustafa@gmail.com>
2020-10-02 23:07:42 +0300

    finish controller builder

--- src/BuildClasses/KMControllerBuilder.php
+++ src/BuildClasses/KMControllerBuilder.php
@@ -5,10 +5,24 @@ namespace KMLaravel\ApiGenerator\BuildClasses;
 
 
 use Illuminate\Support\Facades\Artisan;
+use KMLaravel\ApiGenerator\Facade\KMFileHelper;
 
 class KMControllerBuilder extends KMBaseBuilder
 {
     protected $typeToMake = "Controller";
+    protected $paths = [
+        "{{ model_path }}" => [],
+        "{{ resource_path }}" => [],
+        "{{ request_path }}" => [],
+        "{{ controller_path }}" => [],
+    ];
+    protected $classes = [
+        "{{ model_path }}" => "{{ model_class }}",
+        "{{ resource_path }}" => "{{ resource_class }}",
+        "{{ request_path }}" => "{{ request_class }}",
+        "{{ controller_path }}" => "{{ controller_class }}",
+    ];
+
     /**
      * @param string $option
      * @return KMControllerBuilder
@@ -19,18 +33,55 @@ class KMControllerBuilder extends KMBaseBuilder
         Artisan::call("make:$command $this->fileName $option");
         return $this;
     }
+
     public function controllerReplacer($columns, $options = [])
     {
-//        dd($options);
+        $this->classReplacer($options);
+        $file = KMFileHelper::getFilesFromStubsAsStream("Controller");
+        $fileInApp = app_path("Http/Controllers/$this->fileName.php");
+        $this->pathsAndClassesReplacer($file, $fileInApp);
     }
+
     /**
      * @param $column
      * @param array $options
      * @return mixed
      */
-    public function build($column,$options = [])
+    public function build($column, $options = [])
     {
         $this->controllerReplacer($column, $options);
         return $this->filepath;
+    }
+
+    protected function classReplacer($option)
+    {
+        if (!gettype($option) == 'array') $option = [$option];
+        $this->paths = array_intersect_key($this->paths , $option);
+        foreach ($option as $path => $classNameSpace) {
+            $fullNameSpaceClass = explode('\\', $classNameSpace);
+            $lastWord = array_key_last(explode('\\', $classNameSpace));
+            $className = $this->array_first_value(explode('\\', $fullNameSpaceClass[$lastWord]));
+            $this->paths[$path] = [
+                $classNameSpace => $className,
+            ];
+        }
+    }
+
+    protected function pathsAndClassesReplacer($file, $fileInApp)
+    {
+        $newFile = $file;
+        foreach ($this->paths as $itemToReplace => $values) {
+            $nameSpace = array_key_first($values);
+            $classType = $this->classes[$itemToReplace];
+            $class = $values[$nameSpace];
+            $fileReplaced = str_replace([$itemToReplace , $classType], [$nameSpace, $class], $newFile);
+            $newFile = $fileReplaced;
+        }
+        return file_put_contents($fileInApp, $newFile);
+    }
+
+    protected function array_first_value($array)
+    {
+        return $array[0];
     }
 }
