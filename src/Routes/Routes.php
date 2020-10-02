5544b8c3e08a1f1dc33d54a3010d9035b341aa78
karam mustafa <karam2mustafa@gmail.com>
2020-10-02 23:08:32 +0300

    added middleware

--- src/Routes/Routes.php
+++ src/Routes/Routes.php
@@ -3,14 +3,37 @@
 use Illuminate\Support\Facades\Route;
 
 Route::namespace("KMLaravel\ApiGenerator\Controllers")
-    ->middleware('web')
+    ->middleware(['web'])
     ->group(function () {
-        Route::get('/apisGenerator/index', function () {
-            return view('ApisGenerator/index');
-        })->name('apisGenerator.index');
-        Route::get('/apisGenerator/create', function () {
-            return view('ApisGenerator/create');
-        })->name('apisGenerator.create');
-        Route::post('/apisGenerator/create', "ApisGeneratorController@create")
-            ->name('apisGenerator.create.send');
+        Route::group(["middleware" => getChildMiddleware()], function () {
+            Route::get('/apisGenerator/index', function () {
+                return view('ApisGenerator/index');
+            })->name('apisGenerator.index');
+            Route::get('/apisGenerator/create', function () {
+                return view('ApisGenerator/create');
+            })->name('apisGenerator.create');
+            Route::post('/apisGenerator/create', "ApisGeneratorController@create")
+                ->name('apisGenerator.create.send');
+        });
     });
+function getParentMiddleware()
+{
+    $parent = config('ApisGenerator.middleware');
+    if (isset($parent)) {
+        return array_keys($parent);
+    }
+}
+
+function getChildMiddleware()
+{
+    $parent = config('ApisGenerator.middleware');
+    $data = [];
+    foreach (config('ApisGenerator.middleware') as $item) {
+        foreach ($item as $item1 => $item2) {
+            $data[] = $item2;
+        }
+    }
+    if (isset($parent)) {
+        return $data;
+    }
+}
