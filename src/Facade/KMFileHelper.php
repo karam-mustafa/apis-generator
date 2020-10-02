cbd5d1ad474297856ca92269819e6bcde9a106db
karam mustafa <karam2mustafa@gmail.com>
2020-10-02 23:11:48 +0300

    add facade classes

---
+++ src/Facade/KMFileHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace KMLaravel\ApiGenerator\Facade;
+
+use Illuminate\Support\Facades\Facade;
+
+
+/**
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getFilesFromStubs (string $file)
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getRequestFile (string $file)
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getResourceFile(string $file)
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getModelFile(string $file)
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getMigrationFile(string $file)
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getRequestFileAsStream(string $file)
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getModelFileAsStream(string $file)
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getClassNameSpace(string $type , mixed $className)
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getFilesFromStubsAsStream(string $file)
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getCredentialJsonFilePath()
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper getCredentialJsonFileAsJson()
+ * @method static \KMLaravel\ApiGenerator\Helpers\KMFileHelper setDataToCredentialJsonFile(array $newData)
+ */
+class KMFileHelper extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'KMFileHelper';
+    }
+}
---
+++ src/Helpers/KMFileHelper.php
@@ -0,0 +1,103 @@
+<?php
+
+
+namespace KMLaravel\ApiGenerator\Helpers;
+
+
+use Illuminate\Support\Facades\File;
+use ReflectionException;
+
+class KMFileHelper
+{
+    /**
+     * @param $fileName
+     * @return string
+     */
+    public static function getFilesFromStubs($fileName)
+    {
+        return __DIR__ . "/../Stubs/$fileName.stub";
+    }
+
+    /**
+     * @param $fileName
+     * @return string
+     */
+    public static function getFilesFromStubsAsStream($fileName)
+    {
+        return File::get(__DIR__ . "/../Stubs/$fileName.stub");
+    }
+
+    /**
+     * @param $fileName
+     * @return string
+     */
+    public static function getRequestFile($fileName)
+    {
+        return app_path("Http/Requests/$fileName");
+    }
+
+    /**
+     * @param $fileName
+     * @return string
+     */
+    public static function getResourceFile($fileName)
+    {
+        return app_path("Http/Resources/$fileName");
+    }
+
+    /**
+     * @param $fileName
+     * @return string
+     */
+    public static function getModelFile($fileName)
+    {
+        return app_path("$fileName");
+    }
+
+    /**
+     * @param $fileName
+     * @return string
+     */
+    public static function getRequestFileAsStream($fileName)
+    {
+        return File::get(app_path("Http/Requests/$fileName"));
+    }
+
+    /**
+     * @param $fileName
+     * @return string
+     */
+    public static function getModelFileAsStream($fileName)
+    {
+        return File::get(app_path($fileName));
+    }
+
+    /**
+     * @param $type
+     * @param $className
+     * @return string
+     */
+    public static function getClassNameSpace($type, $className)
+    {
+        if ($type == "model") {
+            return "App\\$className";
+        }
+        return "App\\Http\\$type\\$className";
+    }
+
+    public static function getCredentialJsonFilePath()
+    {
+        return resource_path("Views/ApisGenerator/credential.json");
+    }
+
+    public static function getCredentialJsonFileAsJson()
+    {
+        return json_decode(File::get(self::getCredentialJsonFilePath()));
+    }
+    public static function setDataToCredentialJsonFile($newData)
+    {
+        $data = self::getCredentialJsonFileAsJson();
+        array_push( $data, $newData);
+        File::put(self::getCredentialJsonFilePath() , json_encode($data));
+    }
+}
