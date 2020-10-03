b3dad6d2c41ed9323a9e0eea681da27383710cd2
karam mustafa <karam2mustafa@gmail.com>
2020-10-04 02:05:24 +0300

    refactor code

--- src/asset/create.blade.php
+++ src/asset/create.blade.php
@@ -5,7 +5,8 @@
             @csrf
             <div class="row">
                 <div class="col-md-12"><h3 class="text-center">Made It Easy <span class="exclamation"> &nbsp;!</span>
-                    </h3></div>
+                    </h3>
+                </div>
             </div>
             <hr>
             <div class="row">
@@ -17,25 +18,31 @@
                         </div>
                         <div class="col-md-12">
                             <div class="added_area">
-                                <div class="form-group">
-                                    <label>title</label>
-                                    <input type="text" name="title" class="form-control">
-                                </div>
-                                <hr>
-                                <h5>column in database</h5>
-                                <div class="column_input_area w-100">
-                                    <div class="form-group ">
-                                        <input type="text" class="form-control column_name" placeholder="column name">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>title</label>
+                                            <input type="text" name="title" class="form-control">
+                                        </div>
                                     </div>
-                                    <div class="form-group w-50">
-                                        <select class="form-control column_type">
-                                            @foreach(config('apis_generator.column_type') as $item)
-                                                <option value="{{$item}}">{{$item}}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="add_new_column hvr-shutter-in-vertical process_icon">+</div>
+                                    <div class="col-md-12 mt-4">
+                                        <hr>
+                                        <label>column in database</label>
+                                        <div class="column_input_area w-100">
+                                            <div class="form-group ">
+                                                <input type="text" class="form-control column_name" placeholder="column name">
+                                            </div>
+                                            <div class="form-group w-50">
+                                                <select class="form-control column_type">
+                                                    @foreach(config('apis_generator.column_type') as $item)
+                                                        <option value="{{$item}}">{{$item}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="add_new_column hvr-shutter-in-vertical process_icon">+</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -46,26 +53,54 @@
                     </div>
                 </div>
                 <div class="col-md-12 col-lg-6">
-                    <h5>Options </h5>
-                    <hr>
                     <div class="row">
-                        @foreach(config('apis_generator.build_options') as $name => $option)
-                            <div class="col-md-4 mt-2">
-                                <div class="form-check">
-                                    <input class="form-check-input" checked type="checkbox" name="options[{{$option}}]"
-                                           id="{{$option}}">
-                                    <label class="form-check-label" for="{{$option}}">
-                                        {{$name}}
-                                    </label>
-                                </div>
+                        <div class="col-md-12">
+                            <h5>Options </h5>
+                            <hr>
+                            <div class="row">
+                                @foreach(config('apis_generator.basic_build_options') as $name => $option)
+                                    <div class="col-md-4 mt-2">
+                                        <div class="form-check">
+                                            <input class="form-check-input" checked type="checkbox"
+                                                   name="basic_options[{{$option}}]"
+                                                   id="{{$option}}">
+                                            <label class="form-check-label" for="{{$option}}">
+                                                {{$name}}
+                                            </label>
+                                        </div>
+                                    </div>
+                                @endforeach
                             </div>
-                        @endforeach
+                        </div>
+                        <div class="col-md-12 mt-4 mb-4">
+                            <hr>
+                            <h5>Advanced options </h5>
+                            <hr>
+                            <div class="row">
+                                @foreach(config('apis_generator.advanced_build_options') as $name => $option)
+                                    <div class="col-md-6 mt-2">
+                                        <div class="form-check">
+                                            <input class="form-check-input" checked type="checkbox"
+                                                   name="advanced_options[{{$option}}]"
+                                                   id="{{$option}}">
+                                            <label class="form-check-label" for="{{$option}}">
+                                                {{$name}}
+                                            </label>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
             <hr>
             <div class="row api_details_area"></div>
-            <button class="btn text-white btn-block mt-4 btn-grad">Save</button>
+            <div class="row justify-content-end">
+                <div class="col-md-2">
+                    <button class="btn text-white btn-block mt-4 btn-grad">Save</button>
+                </div>
+            </div>
         </form>
     </div>
 @endsection
