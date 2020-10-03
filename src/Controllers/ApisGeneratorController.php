63098e773d7b4c422084d97e5dcd73173649d828
karam mustafa <karam2mustafa@gmail.com>
2020-10-04 02:03:51 +0300

    update property name to service

--- src/Controllers/ApisGeneratorController.php
+++ src/Controllers/ApisGeneratorController.php
@@ -8,16 +8,16 @@ use KMLaravel\ApiGenerator\Requests\ApisGeneratorRequest;
 
 class apisGeneratorController extends Controller
 {
-    protected $generatorFactory;
+    protected $generatorService;
 
     public function __construct()
     {
-        $this->generatorFactory = new GeneratorService();
+        $this->generatorService = new GeneratorService();
     }
 
     public function create(ApisGeneratorRequest $request)
     {
-        $this->generatorFactory->initialRequest($request)->generateApi();
+        $this->generatorService->initialRequest($request)->generateApi();
         session()->flash('alert_type' , 'success');
         session()->flash('success' , 'create new api successfully');
         return redirect()->route('apisGenerator.index');
