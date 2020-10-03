beddd3a194c66914d951e67e5c9f9e65883864d3
karam mustafa <karam2mustafa@gmail.com>
2020-10-04 02:05:45 +0300

    refactor code and added explanations comment

--- src/Services/GeneratorService.php
+++ src/Services/GeneratorService.php
@@ -9,89 +9,141 @@ use KMLaravel\ApiGenerator\BuildClasses\KMControllerBuilder;
 use KMLaravel\ApiGenerator\BuildClasses\KMModelAndMigrationBuilder;
 use KMLaravel\ApiGenerator\BuildClasses\KMRequestBuilder;
 use KMLaravel\ApiGenerator\Facade\KMFileHelper;
-use KMLaravel\ApiGenerator\Requests\ApisGeneratorRequest;
 
 class GeneratorService
 {
+
     /**
-     * @var
+     * @var object
      */
     private $request;
     /**
-     * @var
+     * @var bool
      */
-    private $checkIfBaseControllerExists;
-    private $baseControllerPath = "Http/Controllers/BaseController.php";
-    private $buildOption;
+    private $baseControllerExists;
+    /**
+     * @var array
+     */
+    private $basicBuildOption;
+    /**
+     * @var array
+     */
+    private $advancedBuildOption;
+    /**
+     * @var string
+     */
     private $apiTitle;
+    /**
+     * @var array
+     */
     private $column;
+    /**
+     * @var array
+     */
     private $paths;
 
+    /**
+     * @param $request
+     * @return GeneratorService
+     * @desc this function initialize request object which has been validated from controller and assigned all data.
+     *
+     */
     public function initialRequest($request): GeneratorService
     {
         $this->request = $request;
         $this->column = $request->column;
         $this->apiTitle = $request->title;
-        $buildOptionWithPrefix = [];
-        foreach ($this->request->options as $item => $status) {
-            $buildOptionWithPrefix[] = $item;
+        $basicBuildOptionWithPrefix = [];
+        $advancedBuildOptionWithPrefix = [];
+        // append all basic options that actor want to run migration  to new array
+        foreach ($this->request->basic_options as $item => $status) {
+            $basicBuildOptionWithPrefix[] = $item;
         }
-        $this->buildOption = $buildOptionWithPrefix;
+        $this->basicBuildOption = $basicBuildOptionWithPrefix;
+        // append all advanced options that actor want to run migration  to new array
+        foreach ($this->request->advanced_options as $item => $status) {
+            $advancedBuildOptionWithPrefix[] = $item;
+        }
+        $this->advancedBuildOption = $advancedBuildOptionWithPrefix;
         return $this;
     }
 
     /**
      * @return GeneratorService
+     * this setter function for baseControllerExists property to determine
+     * if actor choose to run migration  to install base controller before.
      */
-    protected function setCheckIfBaseControllerExists(): GeneratorService
+    public function setBaseControllerExists(): GeneratorService
     {
-        $this->checkIfBaseControllerExists = File::exists(app_path($this->baseControllerPath));
+        $this->baseControllerExists = KMFileHelper::baseControllerExists();
         return $this;
     }
 
     /**
+     * this main function which process is distributed throughout functions available in this current class
+     * the function is check if this function is exists and this function ame came from actor options to run
+     * migration which is loaded from api_generator config file.
      */
     public function generateApi()
     {
-        $this->setCheckIfBaseControllerExists();
-        foreach ($this->buildOption as $item) {
-            if (method_exists(__CLASS__, $item)) {
-                $this->$item();
+        $this->setBaseControllerExists();
+        // merge basic options and advanced options.
+        //we here want to be advanced options last options to run inside foreach.
+        $options = array_merge($this->basicBuildOption , $this->advancedBuildOption);
+        foreach ($options as $option) {
+            if (method_exists(__CLASS__, $option)) {
+                $this->$option();
             }
         }
+        // of course we will build controller as a last step
+        // because now we have all paths we need
         $this->buildController();
+        // at the end we will register some data to credential file in assets
         $this->registerCredential();
     }
 
     /**
-     *
+     * @return \KMLaravel\ApiGenerator\Helpers\KMFileHelper
+     * this function is use separate class its work like a services to auto build model and check from
+     * migration options and finally push the path for model to paths property to use it when we create
+     * controller as final step.
      */
     protected function buildModelAndMigrations()
     {
         $builder = new KMModelAndMigrationBuilder();
         $modelClass = "$this->apiTitle";
-        $migrationRequired = isset($this->buildOption['buildMigration']) ? true : false;
+        $migrationRequired = in_array("buildMigration" , $this->basicBuildOption);
         $builder->initialResource($modelClass, "modelAndMigrationReplacer")
             ->callArtisan($migrationRequired ? '-m' : '')
-            ->build($this->column , ["migrationRequired" => $migrationRequired]);
-        $this->paths["{{ model_path }}"] = KMFileHelper::getClassNameSpace("model", $modelClass);
+            ->build($this->column, ["migrationRequired" => $migrationRequired]);
+        return $this->paths["{{ model_path }}"] = KMFileHelper::getClassNameSpace("model", $modelClass);
     }
 
     /**
      *
      */
-    protected function buildBaseController()
+    protected function runBuildBaseController()
     {
-        if (!$this->checkIfBaseControllerExists) {
-            file_put_contents(
-                app_path($this->baseControllerPath),
-                File::get(KMFileHelper::getFilesFromStubs("BaseController.stub"))
+        if (!$this->baseControllerExists) {
+            file_put_contents(KMFileHelper::baseControllerPath(),
+                File::get(KMFileHelper::getFilesFromStubs("BaseController"))
             );
         }
     }
 
     /**
-     *
+     * if actor choose to run migration.
+     */
+    protected function runMigration()
+    {
+        return Artisan::call("migrate");
+    }
+
+    /**
+     * @return \KMLaravel\ApiGenerator\Helpers\KMFileHelper
+     * this function is used KMRequestBuilder separate class its work like a services to auto build request and
+     * put the rule which actor is selected and finally push the path for model to paths property to use it when
+     * we create controller as final step.
      */
     protected function buildRequests()
     {
@@ -100,40 +152,52 @@ class GeneratorService
         $builder->initialResource($requestClass, "requestReplacer")
             ->callArtisan()
             ->build($this->column);
-        $this->paths["{{ request_path }}"] = KMFileHelper::getClassNameSpace("Requests", $requestClass);
+        return $this->paths["{{ request_path }}"] = KMFileHelper::getClassNameSpace("Requests", $requestClass);
     }
 
+    /**
+     * @return mixed
+     * here we build controller ads final step in building process
+     * the idea is take all paths that resulted from the previous process ( request ,  model , migrations ..)
+     * and its addition to controller path , and then choose  controllerReplacer function to replaces the
+     * resource in controller.stub file and append it to controllers folder.
+     */
     protected function buildController()
     {
         $builder = new KMControllerBuilder();
         $controllerClass = "$this->apiTitle" . "Controller";
-         $builder->initialResource($controllerClass, "controllerReplacer")
+        return $builder->initialResource($controllerClass, "controllerReplacer")
             ->callArtisan()
-            ->build($this->column, array_merge( $this->paths, [
+            ->build($this->column, array_merge($this->paths, [
                 "{{ controller_path }}" => KMFileHelper::getClassNameSpace("Controller", $controllerClass),
             ]));
     }
 
     /**
-     *
+     * this auto generate resources file which is use standard laravel resources file
+     * and get the path do this resource and push them to paths array
      */
     protected function buildResource()
     {
         Artisan::call("make:resource " . "$this->apiTitle" . "Resource");
         return $this->paths["{{ resource_path }}"] = KMFileHelper::getClassNameSpace("Resources", "$this->apiTitle" . "Resource");
     }
+
     /**
-     *
+     * @return \KMLaravel\ApiGenerator\Helpers\KMFileHelper
+     * in this function we can register some information about our process result
+     * like controller name , url ( will be api title as default ) , and title for this api.
+     * @note we will develop route later
      */
     protected function registerCredential()
     {
         $data = [
             $this->apiTitle => [
                 "route" => "$this->apiTitle" . "Controller",
-                "url" =>  "$this->apiTitle",
-                "name" =>  "$this->apiTitle",
+                "url" => "$this->apiTitle",
+                "name" => "$this->apiTitle",
             ]
         ];
-       return KMFileHelper::setDataToCredentialJsonFile($data);
+        return KMFileHelper::setDataToCredentialJsonFile($data);
     }
 }
