44ae1b169eeabc7727699b153f6e2c3504d68601
karam mustafa <karam2mustafa@gmail.com>
2020-10-16 17:26:35 +0300

    move artisan function to base builder class

--- src/BuildClasses/KMModelAndMigrationBuilder.php
+++ src/BuildClasses/KMModelAndMigrationBuilder.php
@@ -3,20 +3,9 @@
 
 namespace KMLaravel\ApiGenerator\BuildClasses;
 
-use Illuminate\Support\Facades\Artisan;
 
 class KMModelAndMigrationBuilder extends KMBaseBuilder
 {
     protected $typeToMake = "Model";
 
-    /**
-     * @param string $option
-     * @return KMModelAndMigrationBuilder
-     */
-    public function callArtisan($option = ''): KMModelAndMigrationBuilder
-    {
-        $command = strtolower($this->typeToMake);
-        Artisan::call("make:$command $this->fileName $option");
-        return $this;
-    }
 }
--- src/BuildClasses/KMRequestBuilder.php
+++ src/BuildClasses/KMRequestBuilder.php
@@ -1,22 +1,8 @@
 <?php
 
-
 namespace KMLaravel\ApiGenerator\BuildClasses;
-
-use Illuminate\Support\Facades\Artisan;
 
 class KMRequestBuilder extends KMBaseBuilder
 {
     protected $typeToMake = "Request";
-
-    /**
-     * @param string $option
-     * @return KMRequestBuilder
-     */
-    public function callArtisan($option = ''): KMRequestBuilder
-    {
-        $command = strtolower($this->typeToMake);
-        Artisan::call("make:$command $this->fileName $option");
-        return $this;
-    }
 }
